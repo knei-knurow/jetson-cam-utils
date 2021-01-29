0ff2400f5db64d49288d5d8235fb454700ec3cea
Bartlomiej <bartlomiej.dudek.3@gmail.com>
2021-01-29 19:38:37 +0100

    First attempts on creating the gst class.

--- opencv/csi/csi.cpp
+++ opencv/csi/csi.cpp
@@ -1,18 +1,61 @@
+#include <iostream>
 #include <string>
-#include <cstdio>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
 
-class gst_camera
+using namespace std;
+
+
+class GStCamera
 {
-	unsigned int id;
-	string gstString;
+    public:
 
-	gst_camera(unsigned int _id, unsigned int _width, unsigned int _height, string fmt)
-	{
-		_id = id;
+    unsigned int id;
+    unsigned int width;
+    unsigned int height;
+    unsigned int framerate;
+    string srcFmt;
+    string sinkFmt;
 
-	gstString = printf("nvarguscamerasrc sensor=id=%d ! \'video/x-raw(memory:NVMM), format=NV11, width=%d, height=%d\' ! nvvidconv ! \'video/x-raw, width=%d, height=%d, format=(string)RGB", _id, _width, _height, _fmt); 
+    string GStString;
+    cv::VideoCapture cap;
+
+    GStCamera(unsigned int id, unsigned int width, unsigned int height, unsigned int framerate, std::string sinkFormat = "BGR", std::string srcFormat="NV12")
+    {
+
+        this->id = id;
+        this->width = width;
+        this->height = height;
+        this->framerate = framerate;
+        this->sinkFmt = sinkFormat;
+        this->srcFmt = srcFormat;
+        
+        stringstream ss;
+
+        ss  << "nvarguscamerasrc sensor=id="
+            << id
+            << " \'video/x-raw(memory:NVMM), format="
+            << srcFormat
+            << ", width="
+            << width
+            << ", height="
+            << height
+            << "\' ! nvvidconv ! \'video/x-raw, width="
+            << width
+            << ", height="
+            << height
+            << ", format=(string)"
+            << sinkFormat
+            << "! appsink ";
+
+        this->cap = cv::VideoCapture(GStString);
 	}
+};
 
 
+
+int main(void)
+{
+    GStCamera cam(0, 1920, 1080, 30, "BGR", "NV12");
 
 }
