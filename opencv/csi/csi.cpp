7af6975193880df51079b1f7afb35ad51b05b05c
Bartlomiej <bartlomiej.dudek.3@gmail.com>
2021-01-31 16:40:32 +0100

    Working GSt pipeline generator added.

--- opencv/csi/csi.cpp
+++ opencv/csi/csi.cpp
@@ -1,76 +1,40 @@
-#include <iostream>
-#include <string>
-#include <unistd.h>
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/imgcodecs.hpp>
+#include "csi.hpp"
+#define CAM_APPSINK (1<<0)
+#define CAM_UDP     (1<<1)
+#define CAM_RTSP    (1<<2)
+#define CAM_TIMEOV  (1<<3)
+#define CAM_ID_OV   (1<<4)
 
 using namespace std;
 
-
-class GStCamera
+GStCamera::GStCamera(unsigned int id, unsigned int width, unsigned int height, unsigned int framerate, unsigned int flags, string sinkFormat = "BGR", string srcFormat="NV12")
 {
-    public:
-
-    unsigned int id;
-    unsigned int width;
-    unsigned int height;
-    unsigned int framerate;
-    string srcFmt;
-    string sinkFmt;
-
-    string GStString;
-    cv::VideoCapture cap;
-
-    GStCamera(unsigned int id, unsigned int width, unsigned int height, unsigned int framerate, std::string sinkFormat = "BGR", std::string srcFormat="NV12")
-    {
-
-        this->id = id;
-        this->width = width;
-        this->height = height;
-        this->framerate = framerate;
-        this->sinkFmt = sinkFormat;
-        this->srcFmt = srcFormat;
+    this->flags = flags;
+    this->id = id;
+    this->width = width;
+    this->height = height;
+    this->framerate = framerate;
+    this->sinkFmt = sinkFormat;
+    this->srcFmt = srcFormat;
         
-        stringstream ss;
-
-        ss  << "nvarguscamerasrc sensor-id="
-            << id
-            << " ! video/x-raw(memory:NVMM), format="
-            << srcFormat
-            << ", width="
-            << width
-            << ", height="
-            << height
-            << " ! nvvidconv ! video/x-raw, width="
-            << width
-            << ", height="
-            << height
-            << ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)"
-            << sinkFormat
-            << " ! videoconvert ! appsink ";
+    stringstream ss;
         
-        GStString = ss.str();      
-        this->cap = cv::VideoCapture(GStString);
-	}
-};
-
-
-
-int main(void)
-{
-
-    cv::Mat img;
-
-    GStCamera cam(0, 1920, 1080, 30, "BGR", "NV12");   
-
-    cv::VideoCapture vc = cam.cap;    
-
-    sleep(5);    
-
-    vc >> img;
-
-    cv::imwrite("read.bmp", img);
-    vc.release();
+    ss  << "nvarguscamerasrc sensor-id="
+        << id
+        << " ! video/x-raw(memory:NVMM), format="
+        << srcFormat
+        << ", width="
+        << width
+        << ", height="
+        << height
+        << " ! nvvidconv ! video/x-raw, width="
+        << width
+        << ", height="
+        << height
+        << ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)"
+        << sinkFormat
+        << " ! videoconvert ! appsink ";
     
+    GStString = ss.str();      
+    this->cap = cv::VideoCapture(GStString);
 }   
